f5d105ee45883c20c6ae8f95b80e25d3bc2841b6
Dmitry Krasun <krasun.net@gmail.com>
2015-03-28 19:43:19 +0200

    Format code

--- go/1/main.go
+++ go/1/main.go
@@ -1,4 +1,4 @@
-package main 
+package main
 
 import "fmt"
 
@@ -6,25 +6,25 @@ func main() {
 	multiples := make(map[int]bool)
 	sum := 0
 	for n, by3, by5 := 1, 3, 5; n < 1000; n++ {
-		
+
 		by3 = n * 3
-		if (by3 >= 1000) {
-			break;
+		if by3 >= 1000 {
+			break
 		}
 
 		_, ok := multiples[by3]
-		if ! ok {
+		if !ok {
 			sum += by3
 			multiples[by3] = true
 		}
 
 		by5 = n * 5
-		if (by5 >= 1000) {	
-			continue;
+		if by5 >= 1000 {
+			continue
 		}
 
 		_, ok = multiples[by5]
-		if ! ok {
+		if !ok {
 			sum += by5
 			multiples[by5] = true
 		}
