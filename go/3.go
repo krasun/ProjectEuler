525ae00bb4252e2e5e523eac6e41abdc816a4690
Dmitry Krasun <krasun.net@gmail.com>
2015-03-30 21:19:40 +0300

    Find the largest prime factor of the number.

---
+++ go/3.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func hasDivider(n int) bool {
+	for divider := 2; divider <= int(math.Sqrt(float64(n))); divider++ {
+		if n%divider == 0 {
+			return true
+		}
+	}
+
+	return false
+}
+
+func divisibleBy(n, divider int) bool {
+	return n%divider == 0
+}
+
+func main() {
+	n := 600851475143
+	maxFactor := int(math.Sqrt(float64(n)))
+
+	for {
+		if divisibleBy(n, maxFactor) && !hasDivider(maxFactor) {
+			break
+		}
+
+		maxFactor--
+	}
+
+	fmt.Println("The largest prime factor of the number %d is %d.\n", n, maxFactor)
+}
