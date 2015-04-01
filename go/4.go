cc017ae83c82b6c35cc10e7e4eda7ed620442c9d
Dmitry Krasun <krasun.net@gmail.com>
2015-04-02 00:02:44 +0300

    Find the smallest positive number, evenly divisible by all of the numbers from 1 to 20.

--- go/4.go
+++ go/4.go
@@ -1,4 +1,4 @@
-package main 
+package main
 
 import (
 	"fmt"
@@ -9,30 +9,29 @@ func isPalindrome(number int) bool {
 	sourceNumber, reverse := number, 0
 	for number > 0 {
 		residue := number % 10
-		reverse = reverse * 10 + residue
+		reverse = reverse*10 + residue
 		number = number / 10
 	}
 
 	return reverse == sourceNumber
 }
 
-func findLargestPalindrome(powFactor int) int {	
+func findLargestPalindrome(powFactor int) int {
 	maxFactor := int(math.Pow(10, float64(powFactor)) - 1)
-	minFactor := int(math.Pow(10, float64(powFactor - 1)))
+	minFactor := int(math.Pow(10, float64(powFactor-1)))
 	largestPalindrome := -1
 
 	for x := maxFactor; x >= minFactor; x-- {
 		for y := maxFactor; y >= minFactor; y-- {
 			product := x * y
-			if (isPalindrome(product) && product >= largestPalindrome) {
+			if isPalindrome(product) && product >= largestPalindrome {
 				largestPalindrome = product
 			}
 		}
-	} 
+	}
 
 	return largestPalindrome
 }
-
 
 func main() {
 	fmt.Printf("The largest palindrome made from the product of two 3-digit numbers is %d.\n", findLargestPalindrome(3))
--- go/5.go
+++ go/5.go
@@ -1,13 +1,50 @@
-package main 
+package main
 
 import (
 	"fmt"
 )
 
-func main() {
-	for i := 1; i <= 10; i++ {
-		fmt.Println(2520 % i)
+func generateDivisibleByAllNumbersUpTo(limit int) int {
+	primes := findPrimes(limit)
+	result := 1
+	for _, prime := range primes {
+		multiplier := prime
+		for {
+			if (prime * multiplier) > limit {
+				break
+			}
+
+			prime *= multiplier
+		}
+
+		result *= prime
 	}
 
-	fmt.Printf("The smallest positive number, evenly divisible by all of the numbers from 1 to 20, is %d.")
+	return result
+}
+
+func findPrimes(limit int) []int {
+	// false value determines prime as key
+	sieve := make([]bool, limit+1)
+
+	// 2 is the smallest prime
+	for p := 2; p <= limit; p++ {
+		for i := 2; i*p <= limit; i++ {
+			sieve[i*p] = true
+		}
+	}
+
+	// filter primes
+	primes := make([]int, 0)
+	for n, isComposite := range sieve {
+		if n >= 2 && !isComposite {
+			primes = append(primes, n)
+		}
+	}
+
+	return primes
+}
+
+func main() {
+	fmt.Printf("The smallest positive number, evenly divisible by all of the numbers from 1 to 20, is %d.\n", generateDivisibleByAllNumbersUpTo(20))
 }
