93174789d23330815d7a867ae8a29c731c269625
Dmitry Krasun <krasun@mserverone.com>
2015-04-01 14:39:22 +0300

    Remove stupid solutions.

--- go/4.go
+++ go/4.go
@@ -5,55 +5,19 @@ import (
 	"math"
 )
 
-func reverse(n int) int {
-	divider := 10	
-		
-	result := 0 
-
-	result += n % divider 
-	n /= divider
-
-	// divider *= 10
-
-	fmt.Println(result)
-	fmt.Println(n)
-	fmt.Println("-----")
-
-	result += (n % divider) * divider
-	n /= divider
-
-
-	fmt.Println(result)
-	fmt.Println(n)
-
-	// result += (result % divider) * divider
-
-	// result += n % 1000	
-
-	return result
-}
-
 func isPalindrome(n int) bool {
-	return n - reverse(n) == 0
+	return true
 }
 
-func findLargestPalindrome(digitNum int) int {
-	limit := int(math.Pow(10, float64(digitNum)) - 1);
-	for x := limit; x >= 0; x-- {
-		for y := limit; y >= 0; y-- {
-			if (isPalindrome(x * y)) {
-				return x * y
-			}
-		}
-	}
-
+func findLargestPalindrome(digitNum int) int {	
 	return 0
 }
 
 func main() {
-	// fmt.Println(reverse(120))
-	reverse(123)
-	// fmts.Println(reverse(1))
+		
+	n := 123
 
+	fmt.Println("length", int(math.Floor(math.Log10(float64(n))) + 1))
+	
 	// fmt.Printf("The largest palindrome made from the product of two 3-digit numbers is %d.\n", findLargestPalindrome(3))
 }
