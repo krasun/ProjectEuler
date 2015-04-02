d8915ac5506c7333433617a9688066f7a4390584
Dmitry Krasun <krasun.net@gmail.com>
2015-04-02 20:00:35 +0300

    Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.

---
+++ go/6.go
@@ -0,0 +1,17 @@
+package main 
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	difference, sum := 0, 0
+	for i := 1; i <= 100; i++ {
+		difference += i * i
+		sum += i
+	}
+	difference = (sum * sum) - difference
+
+	fmt.Printf("The difference between the sum of the squares of the first one hundred natural numbers and the square of the sum is %d.\n", difference)
+}
